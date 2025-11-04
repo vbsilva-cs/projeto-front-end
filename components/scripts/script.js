b0d30a0a13d0464f7acf8caad9dd1fc638e3123c
vbsilva-cs <vinicius.brito.esilva@gmail.com>
2025-11-04 03:28:14 -0300

    feat: Adiciona script para validação de formulario, espaços em branco são rejeitados

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  /* ATIVACAO DO MENU HAMBURGUER TOGGLE */
   const toggle = document.getElementById('menu-toggle');
   const menu = document.getElementById('menu-itens');
   const any = document.querySelector('.main');
@@ -11,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  /* COMPLEMENTO: FECHAR MENU AO AFASTAR O CURSOR DO MENU ABERTO*/
   menu.addEventListener("mouseout", () => {
       any.addEventListener("mouseover", () => {
         if (menu.classList.contains("aberto")) {
@@ -20,4 +23,56 @@ document.addEventListener('DOMContentLoaded', function () {
         }  
       });
     });
-  });
+
+    /* VALIDAÇÃO DE FORMULARIO */
+	const form =  document.querySelector('form');
+    const statusNome = document.querySelector('#nome-status');
+    const statusEndereco = document.querySelector('#endereco-status');
+    const statusCidade = document.querySelector('#cidade-status');
+    const msgEnvio = document.querySelector('#confirmacao');
+
+	if (form) {
+		form.addEventListener('submit', (event) => {
+      event.preventDefault();	
+      const nome = form.querySelector('#nome').value.trim();
+      const endereco = form.querySelector('#endereco').value.trim();
+      const cidade = form.querySelector('#cidade').value.trim();
+
+        if (nome == '') { 
+          statusNome.innerHTML = '<p class="erro">* Campo obrigatório!</p>';
+          statusNome.className = 'status-erro';
+        } else {
+          statusNome.innerHTML = '<p class="sucesso">Okay</p>';
+          statusNome.className = 'status-sucesso';
+        };
+        
+        if (endereco == '') { 
+          statusEndereco.innerHTML = '<p class="erro">* Campo obrigatório!</p>';
+          statusEndereco.className = 'status-erro';
+        } else {
+          statusEndereco.innerHTML = '<p class="sucesso">Okay</p>';
+          statusEndereco.className = 'status-sucesso';
+        };
+
+        if (cidade == '') { 
+          statusCidade.innerHTML = '<p class="erro">* Campo obrigatório!</p>';
+          statusCidade.className = 'status-erro';
+        } else {
+          statusCidade.innerHTML = '<p class="sucesso">Okay</p>';
+          statusCidade.className = 'status-sucesso';
+        };
+
+        if (nome == '' || endereco == '' || cidade == '') {
+          alert('Por favor, preencha todos os campos corretamente.');        
+          msgEnvio.innerHTML = '<p class="erro">Por favor, preencha todos os campos corretamente.</p>';
+          msgEnvio.className = 'status-erro';
+        } else {          
+          msgEnvio.innerHTML = '<p class="sucesso">Formulário enviado com sucesso!</p>';
+          msgEnvio.className = 'status-sucesso';
+          alert('Formulário enviado com sucesso!');
+          form.reset()
+          return;
+      }
+    });
+  } 
+});
